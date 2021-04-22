752a52a5e3508d47d3b59691dc5f6122e057d16b
Malak-Atef <79705774+Malak-Atef@users.noreply.github.com>
2021-04-22 16:34:27 +0200

    the update

--- PAGE.PHOTOS/js/script.js
+++ PAGE.PHOTOS/js/script.js
@@ -1,7 +1,9 @@
-// $('#view-work').on('click', function(){
-//   const images = $('#images').position().top;
 
-//   $('html, body').animate({
-//     scrollTop: images
-//   }, 900);
-// });
+function myFunction(imgs) {
+    var expandImg = document.getElementById("expandedImg");
+    var imgText = document.getElementById("imgtext");
+    expandImg.src = imgs.src;
+    imgText.innerHTML = imgs.alt;
+    expandImg.parentElement.style.display = "block";
+  }
+
