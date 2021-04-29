a69e7719ddd93f14e9665a2ed87f840c1a85acac
Malak-Atef <79705774+Malak-Atef@users.noreply.github.com>
2021-04-29 16:41:55 +0200

    new app v

---
+++ script.js
@@ -0,0 +1,12 @@
+// const goTop = document.getElementById("top");
+// const section = document.getElementById("aboutUs");
+
+// if(section.id == "aboutUs"){   
+                   
+//     goTop.style.display = 'none';
+// }else {
+
+//     goTop.style.display ='block';
+//   };
+
+// console.log("goTop");
