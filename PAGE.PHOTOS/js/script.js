dfcd53284f202809fb1f8d8d975dfc5cd9e5c93f
Malak-Atef <malak.atif12@gmail.com>
2021-05-05 14:59:05 +0200

    new1

--- PAGE.PHOTOS/js/script.js
+++ PAGE.PHOTOS/js/script.js
@@ -7,3 +7,22 @@ function myFunction(imgs) {
     expandImg.parentElement.style.display = "block";
   }
 
+  const goTop = document.getElementById("top");
+
+  window.onscroll = function() {scrollFunction()};
+  
+  function scrollFunction() {
+    if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+      goTop.style.display = "block";
+    } else {
+      goTop.style.display = "none";
+    }
+  }
+  
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
+  
